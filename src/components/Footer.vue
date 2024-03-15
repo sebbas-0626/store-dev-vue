ec0594fbb42bcb06b886b76f459f5b4f0a1ea4fb
sebastian tovar <sebastiantovarchavez304@gmail.com>
2024-03-15 14:10:52 -0500

    :sparkles: componente header y footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <footer>
+        <div class="flex flex-col  justify-center ">
+            <div class="">
+                <h1>AYUDA</h1>
+            </div>
+            <div class="">
+                <h1>ATENCIÓN AL CLIENTE
+                </h1>
+            </div>
+            <div class="">
+                <h1>EMPRESA Y POLÍTICAS
+                </h1>
+            </div>
+            <div class="">
+                <h1>SUSCRÍBETE A NUESTRA NEWSLETTER
+                </h1>
+            </div>
+        </div>
+        <div class=" flex justify-between bg-black text-white px-2 py-6">
+            <h1>© 2024®. Todos los derechos reservados.</h1>
+            <h1> Los precios mostrados en esta web incluyen IVA.</h1>
+        </div>
+    </footer>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style></style>
