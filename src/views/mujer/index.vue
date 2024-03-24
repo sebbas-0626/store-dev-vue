c6d7e899000aa5d8fbaebda0a146db39fca8241b
sebastian tovar <sebastiantovarchavez304@gmail.com>
2024-03-23 23:58:06 -0500

    :sparkles: logos secciones

--- src/views/mujer/index.vue
+++ src/views/mujer/index.vue
@@ -14,6 +14,20 @@
         />
       </div>
     </div>
+    <div class="flex justify-center">
+      <div class="mx-4">
+        <img
+          src="/public/images/hombre/saco1.webp"
+          class="bg-white w-20 h-20 rounded-full border border-gray-300"
+        />
+      </div>
+      <div>
+        <img
+          src="/public/images/hombre/saco2.webp"
+          class="bg-white w-20 h-20 rounded-full border border-gray-300"
+        />
+      </div>
+    </div>
     <section>
       <Cards/>
     </section>
