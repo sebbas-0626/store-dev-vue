c32b66da3e4349480c07250e81657d0c65216b40
sebastian tovar <sebastiantovarchavez304@gmail.com>
2024-04-08 14:25:37 -0500

    :sparkles: feat: vista home principal

--- src/components/home/CardH.vue
+++ src/components/home/CardH.vue
@@ -1,13 +1,14 @@
 <template>
     <div>
-        <div class="w-72 bg-black">
-
-            <a href="https://codevmonkey.com/hombre/" class="gridwide" style="background-image: url(https://codevmonkey.com/wp-content/themes/codevmonkey/images/pages/home_01.jpg);  position:relative; background-repeat:no-repeat; background-size:cover; background-position:center;">
-				<div class="text-white ">
-                    <h1>Hombre</h1>
+        <div class="w-11/12 mt-3 bg-black">
+            <a href="https://codevmonkey.com/hombre/" class="gridwide"
+                style="background-image: url(https://codevmonkey.com/wp-content/themes/codevmonkey/images/pages/home_01.jpg);  position:relative; background-repeat:no-repeat; background-size:cover; background-position:center;">
+                <div class="text-white bg-black h-20 flex justify-center">
+                    <h1 class="text-4xl my-4">Hombre</h1>
                 </div>
-				<img src="https://codevmonkey.com/wp-content/themes/codevmonkey/images/pages/home_01.jpg" class="w-72">
-			</a>
+                <img src="https://codevmonkey.com/wp-content/themes/codevmonkey/images/pages/home_01.jpg"
+                    class="w-full h-auto">
+            </a>
         </div>
     </div>
 </template>
--- src/views/home/novedades.vue
+++ src/views/home/novedades.vue
@@ -1,17 +1,28 @@
 <template>
   <div>
     <section>
-      <h1 class="text-xl sm:text-5xl font-bold flex justify-center mt-14">
+      <h1 class="text-xl sm:text-5xl font-bold flex justify-center mt-10">
         NOVEDADES
       </h1>
-      <CardH/>
+      <section class="grid grid-cols-4 gap-4 mx-10 my-10">
+        <CardH class="col-span-2" />
+        <Card v-for="(product, index) in productosHombres.slice(0, 6)" :key="'hombre-product' + index" :product="product" />
+        <CardH class="col-span-2 " />
+
+        <Card  v-for="(product, index) in productosMujer.slice(0, 6)" :key="'mujer-product' + index" :product="product" />
+
+      </section>
     </section>
   </div>
 </template>
 
 <script setup lang="ts">
+import Card from '@/components/product/Card.vue';
 import CardH from '@/components/home/CardH.vue';
-
+import productosHombres from '@/data/productosHombres.json';
+import productosMujer from '@/data/productosMujer.json';
 </script>
 
-<style></style>
+<style>
+/* Estilos CSS para la vista Home */
+</style>
