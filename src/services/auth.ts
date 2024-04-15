57b16a9f77fa94aeba0fb8fd04b84971b4634b92
sebastian tovar <sebastiantovarchavez304@gmail.com>
2024-04-15 09:17:34 -0500

    :bug: fix auth services

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,11 +2,10 @@ import axios from "axios";
 const apiUrl = import.meta.env.PUBLIC_API_URL;
 
 
-const login = async (data) => {
+const login = async (data: any) => {
     return await axios.post(`${apiUrl}/login`, data).then((res) => res.data);
   };
 
   export default{
     login,
-    
   }
