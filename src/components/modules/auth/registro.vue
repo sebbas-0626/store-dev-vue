8e0dc1cd80a3685c6456ee0092ae5ff51e709b50
sebastian tovar <sebastiantovarchavez304@gmail.com>
2024-04-17 23:39:06 -0500

    :sparkles: feat: logoica registro de usuario

--- src/components/modules/auth/registro.vue
+++ src/components/modules/auth/registro.vue
@@ -1,5 +1,3 @@
-import Registro from '@/components/modules/auth/registro.vue';
-import { RouterLink } from 'vue-router';
 <template>
     <div>
         <div class="">
@@ -11,30 +9,28 @@ import { RouterLink } from 'vue-router';
                             Registrarte
                         </h2>
                         <p class="mt-2 text-base text-gray-600">
-                            Ya tienes una cuenta?
-                            <router-link to="/login">
-                                <a class="font-bold">
-                                    Inicia Sesión
-                                </a>
+                            ¿Ya tienes una cuenta?
+                            <router-link to="/login" class="font-bold">
+                                Inicia Sesión
                             </router-link>
                         </p>
-                        <form class="mt-5">
+                        <form class="mt-5" @submit.prevent="onSubmit">
                             <div class="space-y-4">
                                 <div>
                                     <label class="text-base font-medium text-gray-900">
                                         Usuario *
                                     </label>
                                     <div class="mt-2">
-                                        <input placeholder="Usuario" type="text"
-                                            class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50" />
+                                        <input v-model="dataForm.name" required placeholder="Usuario" type="text"
+                                            class="flex h-10 w-full round ed-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50" />
                                     </div>
                                 </div>
                                 <div>
                                     <label class="text-base font-medium text-gray-900">
-                                        Correo Electronico *
+                                        Correo Electrónico *
                                     </label>
                                     <div class="mt-2">
-                                        <input placeholder="Correo" type="email"
+                                        <input v-model="dataForm.email" required placeholder="Correo" type="email"
                                             class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50" />
                                     </div>
                                 </div>
@@ -45,17 +41,17 @@ import { RouterLink } from 'vue-router';
                                         </label>
                                     </div>
                                     <div class="mt-2">
-                                        <input placeholder="Contraseña" type="password"
+                                        <input v-model="dataForm.password" required placeholder="Contraseña"
+                                            type="password"
                                             class="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50" />
                                     </div>
                                 </div>
                                 <div>
                                     <button
                                         class="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
-                                        type="button">
+                                        type="submit">
                                         Registrarse
                                     </button>
-
                                 </div>
                             </div>
                         </form>
@@ -63,6 +59,41 @@ import { RouterLink } from 'vue-router';
                 </div>
             </section>
         </div>
-
     </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import auth from "@/services/auth";
+import Swal from "sweetalert2";
+
+const initialData = {
+    email: '',
+    password: '',
+    name: '',
+};
+
+const dataForm = ref({ ...initialData });
+
+const onSubmit = () => {
+    auth.register(dataForm.value)
+        .then((response) => {
+            dataForm.value = { ...initialData };
+            Swal.fire({
+                icon: "success",
+                title: "Correcto",
+                text: response.message,
+            });
+            // Redireccionar a la página de inicio de sesión u otra página después del registro exitoso
+            // router.push("/login");
+        })
+        .catch((error) => {
+            const data = error?.response?.data;
+            Swal.fire({
+                icon: "error",
+                title: "Opss..",
+                text: data?.message ?? 'Ocurrió un error inesperado',
+            });
+        });
+}
+</script>
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 const apiUrl = import.meta.env.VITE_API_BASE_URL;
 
-console.log('api', import.meta.env.VITE_API_BASE_URL);
+const register = async (data:any) => {
+  return await axios.post(`${apiUrl}/register`, data).then((res) => res.data);
+};
+
+// console.log('api', import.meta.env.VITE_API_BASE_URL);
 const login = async (data: any) => {
   return await axios.post(`${apiUrl}/login`, data).then((res) => res.data);
 
@@ -9,4 +13,5 @@ const login = async (data: any) => {
 
 export default {
   login,
+  register
 }
