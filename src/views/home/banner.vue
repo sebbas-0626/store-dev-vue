c04b440478dc04fe518dfb24ea6018a329f6111d
sebastian tovar <sebastiantovarchavez304@gmail.com>
2025-06-07 23:04:52 -0500

    fix: :fire: cambiar el tamaño de la img

--- src/views/home/banner.vue
+++ src/views/home/banner.vue
@@ -2,6 +2,7 @@
   <div>
     <section class="w-full h-[400px] overflow-hidden">
       <img src="/images/home/camiseta-banner.jpg" class="w-full h-full object-cover" alt="Banner camiseta">
+      <!-- revizar la el tamaño de la img -->
     </section>
   </div>
 </template>
