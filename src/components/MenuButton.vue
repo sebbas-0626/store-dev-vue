0b3c2fd9dd7972e0ce2d1f389dd8635d4d3e5e1d
sebastian tovar <sebastiantovarchavez304@gmail.com>
2025-11-04 23:28:58 -0500

    feat: :sparkles: creando un componente

---
+++ src/components/MenuButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="text-gray-700 sm:hidden" @click="$emit('toggle')">
+    <svg v-if="!isOpen" class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+      <path d="M 3 8 A 2.0002 2.0002 0 1 0 3 12 L 47 12 A 2.0002 2.0002 0 1 0 47 8 L 3 8 z M 3 23 A 2.0002 2.0002 0 1 0 3 27 L 47 27 A 2.0002 2.0002 0 1 0 47 23 L 3 23 z M 3 38 A 2.0002 2.0002 0 1 0 3 42 L 47 42 A 2.0002 2.0002 0 1 0 47 38 L 3 38 z"></path>
+    </svg>
+    <svg v-else class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+      <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+    </svg>
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  isOpen: boolean;
+}>();
+
+defineEmits<{
+  toggle: [];
+}>();
+</script>
