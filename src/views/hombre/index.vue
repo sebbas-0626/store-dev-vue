dbc5d676feee42f457956e5d32c2468cfe57db24
sebastian tovar <sebastiantovarchavez304@gmail.com>
2026-01-18 23:43:52 -0500

    feat: :sparkles: vista productos hombres

--- src/views/hombre/index.vue
+++ src/views/hombre/index.vue
@@ -3,12 +3,13 @@
     <AvatarGallery :avatars="avatars" />
     <section>
       <section class="grid grid-cols-2 sm:grid-cols-4 gap-4 mx-7 my-7">
-        <router-link :to="`/producto/${productItem.slug}`" v-for="(productItem, index) in products" :key="productItem.slug">
+        <router-link :to="`/producto/${productItem.slug}`" v-for="(productItem, index) in products"
+          :key="productItem.slug">
           <Card :product="productItem" />
         </router-link>
       </section>
     </section>
-    
+
   </div>
 </template>
 
