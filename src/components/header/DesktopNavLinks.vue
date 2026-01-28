b6afec60d5852075344f51f99089bd3533687fbb
sebastian tovar <sebastiantovarchavez304@gmail.com>
2026-01-28 00:41:37 -0500

    feat: :fire: Menú Desktop

--- src/components/header/DesktopNavLinks.vue
+++ src/components/header/DesktopNavLinks.vue
@@ -1,16 +1,22 @@
 <template>
-    <!-- Menú Desktop -->
-    <div class="flex-1 hidden sm:flex justify-center items-center w-full">
-        <div class="flex gap-4">
-            <div class="text-gray-950 hover:bg-gray-200 px-4 py-2 rounded hover:text-gray-900">
-                <router-link to="/mujer">MUJER</router-link>
-            </div>
-            <div class="text-gray-950 hover:bg-gray-200 px-4 py-2 rounded hover:text-gray-900">
-                <router-link to="/hombre">HOMBRE</router-link>
-            </div>
-            <div class="text-gray-950 hover:bg-gray-200 px-4 py-2 rounded hover:text-gray-900">
-                <router-link to="/accesorios">ACCESORIOS</router-link>
-            </div>
-        </div>
+  <!-- Menú Desktop -->
+  <div class="flex-1 hidden sm:flex justify-center items-center w-full">
+    <div class="flex gap-4">
+      <div
+        class="text-gray-950 hover:bg-gray-200 px-4 py-2 rounded hover:text-gray-900"
+      >
+        <router-link to="/mujer">MUJER</router-link>
+      </div>
+      <div
+        class="text-gray-950 hover:bg-gray-200 px-4 py-2 rounded hover:text-gray-900"
+      >
+        <router-link to="/hombre">HOMBRE</router-link>
+      </div>
+      <div
+        class="text-gray-950 hover:bg-gray-200 px-4 py-2 rounded hover:text-gray-900"
+      >
+        <router-link to="/accesorios">ACCESORIOS</router-link>
+      </div>
     </div>
+  </div>
 </template>
