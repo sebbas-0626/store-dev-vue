340c3e4ff211f2988cbe4ed640f957efc5435d1b
sebastian tovar <sebastiantovarchavez304@gmail.com>
2026-02-18 23:52:14 -0500

    feat: :sparkles: se importo el componente productgrid

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,9 +2,11 @@
   <!-- <h1>secciones </h1> -->
   <banner />
   <novedadesVue />
+  <ProductGrid />
 </template>
 
 <script setup lang="ts">
+import ProductGrid from "@/components/home/ProductGrid.vue";
 import banner from "./banner.vue";
 import novedadesVue from "./novedades.vue";
 
