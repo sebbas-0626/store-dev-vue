875be15d3e6bd710b317e31137315be44d7494e4
sebastian tovar <sebastiantovarchavez304@gmail.com>
2026-02-21 23:14:17 -0500

    fix: :recycle: re estructurando la estructura del proyecto

--- src/types/AuthPayload.ts
+++
@@ -1,5 +0,0 @@
-export interface AuthPayload {
-  email: string;
-  password: string;
-  // Add other fields if needed
-}
--- src/types/Product.ts
+++
@@ -1,9 +0,0 @@
-export interface Product {
-    image: string;
-    title: string;
-    price: string;
-    slug?: string;
-    type?: string;
-    colors?: Array<{ name: string; class: string }>;
-    sizes?: Array<{ name: string }>;
-}
